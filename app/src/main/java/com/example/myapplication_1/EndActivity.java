4e420ded3dea02493e3e965b2dd4f4d0c593eb81
okju <okju031205@naver.com>
2024-11-05 13:38:28 +0900

    main challenge, timer, end activity

---
+++ app/src/main/java/com/example/myapplication_1/EndActivity.java
@@ -0,0 +1,38 @@
+package com.example.myapplication_1;
+
+import android.os.Bundle;
+import android.view.View;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class EndActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_challenge);
+
+        // 설정 버튼 클릭 리스너
+        findViewById(R.id.settingsButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // 설정 버튼 클릭 시 동작
+            }
+        });
+
+        // Yes 버튼 클릭 리스너
+        findViewById(R.id.yesButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Yes 버튼 클릭 시 동작
+            }
+        });
+
+        // No 버튼 클릭 리스너
+        findViewById(R.id.noButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // No 버튼 클릭 시 동작
+            }
+        });
+    }
+}
