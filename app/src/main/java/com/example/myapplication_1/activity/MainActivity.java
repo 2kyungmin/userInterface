547368376e6263de852307f983354dafc0a9343d
okju <okju031205@naver.com>
2024-11-22 14:02:01 +0900

    11/22

--- app/src/main/java/com/example/myapplication_1/activity/MainActivity.java
+++ app/src/main/java/com/example/myapplication_1/activity/MainActivity.java
@@ -29,7 +29,7 @@ public class MainActivity extends AppCompatActivity {
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                Intent intent = new Intent(MainActivity.this, SignupActivity.class);
                 startActivity(intent);
             }
         });
